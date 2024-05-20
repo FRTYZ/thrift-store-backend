7abb04b9892b0e88bdc93b427d8c33a038bb9a4b
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-05-20 18:47:18 +0300

    Edited allowedOrigins

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
     'http://localhost:5173',
-    'http://localhost:8080'
+    'http://localhost:8080',
+    'https://thrift-store-web.vercel.app'
 ];
 
 module.exports = allowedOrigins;

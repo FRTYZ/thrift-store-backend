8d7ffb7c6fb958eb49d462ca3d0e9a08c78c41c1
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-05-20 18:54:12 +0300

    Edited fireabase.ts for bug

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
+const privateKey: any = process.env.FIREBASE_PRIVATE_KEY
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY as string
+    privateKey: privateKey.replace(/\\n/g, '\n')
   }),
   storageBucket: process.env.FIREBASE_STORAGE
 })

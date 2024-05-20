17daff51b48ac868a48ef665a786bf22eb531a8f
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-05-20 18:28:41 +0300

    Edited firebase.ts

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -4,9 +4,9 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
+    privateKey: process.env.FIREBASE_PRIVATE_KEY as string
   }),
-  storageBucket:  process.env.FIREBASE_STORAGE
+  storageBucket: process.env.FIREBASE_STORAGE
 })
 
 const bucket = admin.storage().bucket()
